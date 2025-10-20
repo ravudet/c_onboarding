e87b7e442670862d6dca6bd5e40cd82927234377
ravudet <170215607+ravudet@users.noreply.github.com>
2025-10-19 17:06:21 -0700

    asdf

--- Project1/hello.c
+++ Project1/hello.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+void print(int x)
+{
+	printf("Integer:");
+	printf(x);
+	printf("\n");
+}
+
+/*void print(double x)
+{
+	printf("Double:");
+	printf(x);
+	printf("\n");
+}*/
+
 int main()
 {
-	printf("Hello, World!\n"); 
+	print(5);
+	print((int)3.14);
 	return 0; 
 }
