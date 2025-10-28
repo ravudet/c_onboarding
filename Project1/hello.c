66a2468142ef84a87680ffc83c7fe6042345244e
ravudet <170215607+ravudet@users.noreply.github.com>
2025-10-28 11:13:32 -0700

    asdf

--- Project1/hello.c
+++ Project1/hello.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <string.h>
 
 void print(int x)
 {
@@ -14,15 +16,47 @@ void print(int x)
 	printf("\n");
 }*/
 
-void writeToFile(char path[], char contents[])
+// https://pubs.opengroup.org/onlinepubs/9699919799/functions/fclose.html
+
+int writeToFile(const char path[], const char contents[])
 {
-	int pathLength = sizeof(path) / sizeof(path[0]);
+	FILE* fptr = fopen(path, "a");
+	if (fptr == NULL)
+	{
+		return errno; //// TODO i think this returns the pointer to the _errno function or something
+	}
+
+	int error = fprintf(fptr, contents);
+	if (error < 0)
+	{
+		return errno;
+	}
+
+	error = fclose(fptr);
+	if (error != 0)
+	{
+		return errno;
+	}
+
+	return 0;
+
+
+
+
+	print(5);
+
+	int a = 5;
+
 	//// TODO write to a file, then write a unit test to confirm that the file was written
 }
 
 int main()
 {
-	print(5);
-	print((int)3.14);
-	return 0; 
+	int error = writeToFile("c:\\users\\ravud\\test.txt", "contents");
+	if (error != 0)
+	{
+		printf("ERROR! %d", strerrorname_np());
+	}
+
+	return 0;
 }
