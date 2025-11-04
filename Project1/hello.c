5739140cae74fa2f8845ae026f5211de16992ef3
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-04 11:00:22 -0800

    asdf

--- Project1/hello.c
+++ Project1/hello.c
@@ -20,10 +20,18 @@ void print(int x)
 
 int writeToFile(const char path[], const char contents[])
 {
-	FILE* fptr = fopen(path, "a");
+	FILE* fptr;
+	
+	fptr = fopen(path, "a");
 	if (fptr == NULL)
 	{
-		return errno; //// TODO i think this returns the pointer to the _errno function or something
+		int error = errno;
+		if (error == 2)
+		{
+			//// TODO create the directory and then loop
+		}
+
+		return error; //// TODO i think this returns the pointer to the _errno function or something
 	}
 
 	int error = fprintf(fptr, contents);
@@ -52,11 +60,14 @@ int main()
 {
 	//// TODO make is so that you can use a path that doesn't exist
 	//// TODO write a unit test to confirm that the file was written
-	int error = writeToFile("c:\\users\\ravud\\test.txt", "contents");
+	int error = writeToFile("c:\\users\\ravud\\test\\test.txt", "contents");
 	if (error != 0)
 	{
 		printf("ERROR! %d", error); //// TODO https://learn.microsoft.com/en-us/cpp/c-runtime-library/errno-constants?view=msvc-170 or you can just look directly in `errno.h`
 	}
+
+	//// TODO https://stackoverflow.com/questions/13993742/is-there-any-way-to-peek-at-the-stdin-buffer
+	int c = fgetc(stdin);
 
 	return 0;
 }
