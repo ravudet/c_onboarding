5839537eb36d131db3460b751181aea555d024f5
ravudet <170215607+ravudet@users.noreply.github.com>
2025-11-05 10:04:43 -0800

    asdf

--- Project1/hello.c
+++ Project1/hello.c
@@ -1,6 +1,14 @@
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#if defined _WIN32
+#include <direct.h>
+#elif defined __linux__
+#include <sys/stat.h>
+#endif
 
 void print(int x)
 {
@@ -18,16 +26,62 @@ void print(int x)
 
 // https://pubs.opengroup.org/onlinepubs/9699919799/functions/fclose.html
 
-int writeToFile(const char path[], const char contents[])
+bool substrcmp(const char x[], const int xIndex, const char y[], const int yIndex, const int length)
 {
-	FILE* fptr;
-	
-	fptr = fopen(path, "a");
+	return memcmp(&x + xIndex, &y + yIndex, length) == 0;
+}
+
+int getParentDirectory(const char path[], const int endIndex, const char directoryDelimiter[], const int delimiterLength)
+{
+	int parentIndex = endIndex;
+	if (substrcmp(path, endIndex - delimiterLength + 1, directoryDelimiter, 0, delimiterLength))
+	{
+		parentIndex = endIndex - delimiterLength + 1;
+	}
+
+	while (endIndex >= 0 && !substrcmp(path, endIndex - delimiterLength + 1, directoryDelimiter, 0, delimiterLength))
+	{
+		--parentIndex;
+	}
+
+	return parentIndex;
+}
+
+int mksubdir(const char path[], int endIndex)
+{
+	char* subPath = malloc(endIndex);
+	if (subPath == NULL)
+	{
+		return errno;
+	}
+
+	memcpy(subPath, path, endIndex);
+
+	int error = mkdir(subPath);
+	return error;
+}
+
+int writeToFile(char path[], int pathLength, const char contents[])
+{
+	//// TODO separating the declaration from the initialization causes an error for some reason
+	//// FILE* fptr;
+	//// fptr = fopen(path, "a");
+
+	FILE* fptr = fopen(path, "a");
 	if (fptr == NULL)
 	{
 		int error = errno;
 		if (error == 2)
 		{
+			int otherError = -1;
+			int parentDirectoryEndIndex = pathLength - 1;
+			while (otherError != 0)
+			{
+				char delimiter[] = "//";
+				parentDirectoryEndIndex = getParentDirectory(path, parentDirectoryEndIndex, "//", sizeof(delimiter) / sizeof(delimiter[0]));
+				otherError = mksubdir(path, parentDirectoryEndIndex);
+			}
+
 			//// TODO create the directory and then loop
 		}
 
@@ -60,7 +114,8 @@ int main()
 {
 	//// TODO make is so that you can use a path that doesn't exist
 	//// TODO write a unit test to confirm that the file was written
-	int error = writeToFile("c:\\users\\ravud\\test\\test.txt", "contents");
+	char path[] = "c:\\users\\ravud\\test\\test2\\test.txt";
+	int error = writeToFile(path, sizeof(path) / sizeof(path[0]), "contents");
 	if (error != 0)
 	{
 		printf("ERROR! %d", error); //// TODO https://learn.microsoft.com/en-us/cpp/c-runtime-library/errno-constants?view=msvc-170 or you can just look directly in `errno.h`
