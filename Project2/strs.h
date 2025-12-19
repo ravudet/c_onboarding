f00a32af7d0909ff42d6a7f6268a4f6c6e36ffb5
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-19 13:48:36 -0800

    asdf

--- Project2/strs.h
+++ Project2/strs.h
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <stdbool.h>
-#include <string.h>
 
 #include "errors.h"
+
+enum substringError
+{
+	substringError_BUG = 1,
+	substringError_ENOMEM,
+};
+enum substringError substring(const char string[], const int endIndex, char* substringPointer[]);
 
 bool substrcmp(const char x[], const int xStart, const char y[], const int yStart, const int length);
