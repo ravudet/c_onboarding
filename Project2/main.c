3f5bd1dfaff55c7586de5ff121d54a2c84c71a5f
ravudet <170215607+ravudet@users.noreply.github.com>
2025-12-19 14:10:05 -0800

    asdf

--- Project2/main.c
+++ Project2/main.c
@@ -47,7 +47,6 @@ enum mainError
 
 enum mainError main()
 {
-	//// TODO separate into a few files
 	//// TODO put array length macro in its own file
 	//// TODO some of your error codes aren't actually possible (for example, EINVAL in the `writeToFile` can't come from `fopen`)
 	//// TODO write a unit test to confirm that the file was written
