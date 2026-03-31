6ae22aac2449050d0308be674c01f9fc9f86dd7c
ravudet <170215607+ravudet@users.noreply.github.com>
2026-03-31 08:51:48 -0700

    asdf

--- Project2/directories_private.h
+++ Project2/directories_private.h
@@ -2,7 +2,14 @@
 
 #include "directories.h"
 
+#if defined _WIN32
+#include <direct.h>
+#elif defined __linux__
+#include <sys/stat.h>
+#endif
+
 #include <errno.h>
+#include <stdlib.h>
 
 #include "errors.h"
 #include "strs.h"
