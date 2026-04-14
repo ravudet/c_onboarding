1e85100816bc421270da0c2c9965cc80456047ee
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-14 13:02:17 -0700

    asdf

--- Project2/main.c
+++ Project2/main.c
@@ -20,7 +20,8 @@
 //// TODO this doesn't actually check for other pointer types? 
 //// TODO handle unsupported types: https://iifx.dev/en/articles/460125220/generic-macro-hacking-achieving-type-safety-and-custom-error-messages-in-c
 #define LENGTH(array) _Generic(array, \
-	char[35]: ARRAY_LENGTH(array) \
+	char[35]: ARRAY_LENGTH(array), \
+	char[]: ARRAY_LENGTH(array) \
 )
 
 
@@ -68,21 +69,38 @@
 
 
 
+#define FIRST(a, ...) a
+#define SECOND(a, b, ...) b
+
+#define EMPTY()
+
+#define EVAL1(...) __VA_ARGS__
+#define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
+#define EVAL4(...) EVAL2(EVAL2(__VA_ARGS__))
+#define EVAL8(...) EVAL4(EVAL4(__VA_ARGS__))
+#define EVAL16(...) EVAL8(EVAL8(__VA_ARGS__))
+#define EVAL32(...) EVAL16(EVAL16(__VA_ARGS__))
+#define EVAL64(...) EVAL32(EVAL32(__VA_ARGS__))
+#define EVAL128(...) EVAL64(EVAL64(__VA_ARGS__))
+#define EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
+#define EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
+#define EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
+#define EVAL(...) EVAL1024(__VA_ARGS__)
+
+#define DEFER1(m) m EMPTY()
+#define DEFER2(m) m EMPTY EMPTY()()
+#define DEFER3(m) m EMPTY EMPTY EMPTY()()()
+#define DEFER4(m) m EMPTY EMPTY EMPTY EMPTY()()()()
+
+#define IS_PROBE(...) SECOND(__VA_ARGS__, 0)
+#define PROBE() ~, 1
 
 #define CAT(a,b) a ## b
 
-
-
-#define SECOND(a, b, ...) b
-#define IS_PROBE(...) SECOND(__VA_ARGS__, 0)
-#define PROBE() ~, 1
 #define NOT(x) IS_PROBE(CAT(_NOT_, x))
 #define _NOT_0 PROBE()
+
 #define BOOL(x) NOT(NOT(x))
-
-
-
-
 
 #define IF_ELSE(condition) _IF_ELSE(BOOL(condition))
 #define _IF_ELSE(condition) CAT(_IF_, condition)
@@ -93,16 +111,42 @@
 #define _IF_1_ELSE(...)
 #define _IF_0_ELSE(...) __VA_ARGS__
 
+#define HAS_ARGS(...) BOOL(FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)())
+#define _END_OF_ARGUMENTS_() 0
+
+#define MAP(m, first, ...)           \
+  m(first)                           \
+  IF_ELSE(HAS_ARGS(__VA_ARGS__))(    \
+    DEFER2(_MAP)()(m, __VA_ARGS__)   \
+  )(                                 \
+    /* Do nothing, just terminate */ \
+  )
+#define _MAP() MAP
+
+#define GREET(x) Hello, x!
 
 
 
-#define EMPTY()
-#define A(n) I like the number n
+
+
+/*#define NATURALS_0 0
+#define NATURALS_1 1, NATURALS_0
+#define NATURALS_2 2, NATURALS_1
+#define NATURALS_3 3, NATURALS_2
+#define NATURALS_4 4, NATURALS_3
+#define NATURALS_5 5, NATURALS_4
+#define NATURALS_6 6, NATURALS_5
+#define NATURALS_7 7, NATURALS_6
 
 
 
+#define _THE_PATTERN(n) char[n]: ARRAY_LENGTH(array) 
+#define THE_PATTERN(n) IF_ELSE(n)(_THE_PATTERN(n), )()*/
 
-#define EVAL1(...) __VA_ARGS__
+//#define PATTERNS(n) \
+//	IF_ELSE(n)
+
+//#define _PATTERNS() PATTERNS
 
 
 
@@ -150,26 +194,28 @@ enum mainError
 
 
 
-int first()
-{
-	return 0;
-}
-
-int second()
-{
-	return 0;
-}
-
-
 enum mainError main()
 {
+	////THE_PATTERN(4);
 
-	IF_ELSE(123)(first)(second);
-	EVAL1(A EMPTY() (123))
+	EVAL(MAP(GREET, Mum, Dad, Adam, Joe))
 
 
 
-	EVAL(FOR_EACH(PRINT_DOUBLE_HELPER))
+	char path2[] = "c:\\users\\ravud\\test\\test2\\test.tx";
+	if (0)
+	{
+		int length = LENGTH(path2);
+	}
+	else
+	{
+		int length = LENGTH(path2);
+	}
+
+	//EVAL1(A EMPTY() (123))
+
+
+	//EVAL(FOR_EACH(PRINT_DOUBLE_HELPER))
 
 
 
