a79d322f2997ccfa57f1c3471f066025904152ea
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-28 09:31:22 -0700

    asdf

--- Project2/main.c
+++ Project2/main.c
@@ -39,7 +39,6 @@ enum mainError
 
 enum mainError main()
 {
-	//// TODO add a private header for arraylength
 	//// TODO make sure you still have the spacing correct in this file (compare it to your other files)
 	//// TODO some of your error codes aren't actually possible (for example, EINVAL in the `writeToFile` can't come from `fopen`)
 	//// TODO write a unit test to confirm that the file was written
--- Project3/arraylength.h
+++ Project3/arraylength.h
@@ -1,3 +1,5 @@
 #pragma once
 
+// NOTE: no private header file for macros because these are all hard dependencies; there are no macro "prototypes" that you define in the public header only
+
 #define ARRAY_LENGTH(array) (sizeof(array) / sizeof(array[0]))
