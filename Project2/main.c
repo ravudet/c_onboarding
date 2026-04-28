010867be424e66afd24cb9ee757761394cefdc92
ravudet <170215607+ravudet@users.noreply.github.com>
2026-04-28 09:27:49 -0700

    asdf

--- Project2/main.c
+++ Project2/main.c
@@ -39,9 +39,6 @@ enum mainError
 
 enum mainError main()
 {
-	//// TODO get the shared libraries working
-	//// TODO is there anything that actually makes the private headers private?
-	//// TODO https://learn.microsoft.com/en-us/cpp/build/adding-references-in-visual-cpp-projects?view=msvc-170
 	//// TODO add a private header for arraylength
 	//// TODO make sure you still have the spacing correct in this file (compare it to your other files)
 	//// TODO some of your error codes aren't actually possible (for example, EINVAL in the `writeToFile` can't come from `fopen`)
