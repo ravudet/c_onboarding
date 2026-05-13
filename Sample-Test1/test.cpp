8905fa839a602ed8d401409ed80a03e182a1a099
ravudet <170215607+ravudet@users.noreply.github.com>
2026-05-13 14:07:25 -0700

    asdf

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,38 @@
 #include "pch.h"
 
+#if defined _WIN32
+#include <direct.h>
+#define getcwd _getcwd
+#elif defined __linux__
+#include <unistd.h>
+#endif
+
+TEST(WriteToFile, WriteToFile)
+{
+	char* workingDirectory = getcwd(NULL, 0);
+
+	std::cout << "Asdf";
+
+	free(workingDirectory);
+
+	//EXPECT_TRUE(false);
+}
+
+
+
+
+
+// TODO Tools > Options > Test Adapter for Google Test -> print test output
+
+
+
+
+
+
+
+
+
+
 TEST(TestCaseName1, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
